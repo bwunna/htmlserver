c25d9f57502999adb5caa0272c2f77e8d9a6d64f
bwunna <brynovivan@yandex.ru>
2023-10-11 15:12:37 +0300

    now works with db

--- Internal/App/Services/ServerStarter/StartServer.go
+++ Internal/App/Services/ServerStarter/StartServer.go
@@ -2,10 +2,16 @@ package ServerStarter
 
 import (
 	"SimpleServer/Internal/App/Controllers/Controller"
+	"SimpleServer/Internal/App/Providers/Provider"
 	"time"
 )
 
 func StartServer() {
 	Server := Controller.NewServer("localhost:8080", nil)
-	Server.Start(time.Minute, time.Minute*2, false)
+	db, err := Provider.NewDataBase("localhost", "postgres", "9340fk3__132AA@", "company", "postgres", 5432)
+	if err != nil {
+		panic(err.Error())
+	}
+	Server.Start(time.Minute, time.Minute*10, false, db, time.Minute*2)
+
 }
