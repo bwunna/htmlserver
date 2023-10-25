34f681478508ca49ec01f654c044d97b6cea69b0
bwunna <brynovivan@yandex.ru>
2023-10-26 01:38:13 +0300

    ties between api and providers

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,8 +1,68 @@
 package controller
 
-// func createUser(*delivery.User)  {
-// providers.db.getUser
-// providers.cache.getTime
-// providers.cache.cleanUser
-// helpers.convert
-//}
+import (
+	"SimpleServer/internal/models"
+	"SimpleServer/internal/providers/cache"
+	"fmt"
+	"time"
+)
+
+type Controller struct {
+	cache *cache.Cache
+}
+
+func NewController(cache *cache.Cache) *Controller {
+	// constructor for controller
+	return &Controller{cache: cache}
+}
+
+func (c *Controller) UpdateSalary(name string) error {
+	// updating salary for user
+	err := c.cache.AskForPromotion(name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (c *Controller) GetUser(name string) (string, error) {
+	// checking if user exists
+	userData, err := c.cache.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	salaryData, err := c.cache.GetSalaryData(name)
+	if err != nil {
+		return "", err
+	}
+	info := fmt.Sprint(*userData, salaryData)
+
+	return info, nil
+}
+
+func (c *Controller) DeleteUser(name string) error {
+	// checking for valid key
+	err := c.cache.Delete(name)
+	// checking for successful deleting
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (c *Controller) AddUser(user *models.User) error {
+	// trying to add user to cache or update info about him
+	err := c.cache.Set(user, time.Minute*10)
+	if err != nil {
+		// checking for errors while adding the user
+		if err = c.cache.Update(user); err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
