2ff624bffec152ec13be6e784de8fb0ac6e0cdea
bwunna <brynovivan@yandex.ru>
2023-10-25 21:08:19 +0300

    same logic

--- internal/delivery/grpcserver.go
+++ internal/delivery/grpcserver.go
@@ -1,8 +1,13 @@
 package delivery
 
 import (
-	"SimpleServer/internal/app/providers/Provider"
 	v1 "SimpleServer/internal/delivery/v1"
+	"SimpleServer/internal/providers/cache"
+	"SimpleServer/internal/providers/db"
+	"SimpleServer/pkg/usersService"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
 	"time"
 )
 
@@ -11,7 +16,38 @@ import (
 // фукнция запуска сервиса
 //
 
-func CreateAndRunUserCenter(address string, defaultExpiration time.Duration, cleanUpInterval time.Duration, endlessLifeTimeAvailability bool, db *Provider.DataBase, promotionInterval time.Duration) *v1.Server {
-	return nil
+const (
+	host       = "localhost"
+	port       = 5432
+	user       = "postgres"
+	password   = "9340fk3__132AA@"
+	dbName     = "company"
+	driverName = "postgres"
+)
 
+func RunGRPCServer() error {
+	dataBase, err := db.NewDB(host, user, password, dbName, driverName, port)
+	if err != nil {
+		return fmt.Errorf("бд")
+	}
+	newCache := cache.NewCache(time.Minute*10, time.Minute*2, false, dataBase, time.Minute*2)
+	server := v1.NewGrpcServer(newCache)
+
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return err
+	}
+	grpcSrv := grpc.NewServer()
+	usersService.RegisterUserCenterServer(grpcSrv, server)
+
+	err = grpcSrv.Serve(lis)
+	if err != nil {
+		return fmt.Errorf("сёрв")
+	}
+	return nil
 }
+
+//func CreateAndRunUserCenter(address string, defaultExpiration time.Duration, cleanUpInterval time.Duration, endlessLifeTimeAvailability bool, db *Provider.DataBase, promotionInterval time.Duration) *v1.Server {
+//	return nil
+//
+//}
